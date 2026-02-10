e6adb579c7a9a2b95a1d192fc9bd98c575e480a9
sarra <sarrasouidi28@gmail.com>
2026-02-10 13:43:30 +0100

    evaluation

---
+++ src/Repository/EvaluationRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Evaluation;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Evaluation>
+ *
+ * @method Evaluation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Evaluation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Evaluation[]    findAll()
+ * @method Evaluation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EvaluationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Evaluation::class);
+    }
+
+    public function save(Evaluation $entity, bool $flush = false): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function remove(Evaluation $entity, bool $flush = false): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+}
